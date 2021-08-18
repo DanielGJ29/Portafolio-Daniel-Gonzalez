c9d2c28e3214cfd3f5d27c89bbdeb4a5246a893b
Daniel <daniel.zam29@gmail.com>
2021-08-18 15:32:37 -0500

    mi segundo commit

---
+++ assets/js/javascript.js
@@ -0,0 +1,93 @@
+document.addEventListener("scroll", function scrollPage(){
+    scrollNavbar();
+    scrollSection();
+    scrollUp();  
+});
+//document.onscroll = scrollPage;
+
+//SCROLL NAVBAR
+const navbar = document.getElementById("navbar");
+
+function scrollNavbar (){
+    
+    if(window.scrollY>150){
+        navbar.classList.add("fixed-nav");
+    }else{
+        navbar.classList.remove("fixed-nav");
+    }
+}
+
+
+//NAVBAR OPEN/CLOSE
+const bars = document.getElementById("bars");
+const nav = document.getElementById("nav-item");
+const close = document.getElementById("close");
+
+function clickDown(e){
+  
+    if(nav.classList.toggle("nav-item-down")){
+        bars.classList.remove("fa-bars");
+        bars.classList.add("fa-times");
+    }else{
+        bars.classList.remove("fa-times");
+        bars.classList.add("fa-bars");
+    }
+}
+bars.onclick = clickDown;
+
+
+//SCROLL SECTION / NAV ITEM ACTIVE
+const sections = document.querySelectorAll("section");
+const link = document.querySelectorAll("#nav-item a");
+
+function scrollSection(){
+    let nameSection = "";
+    sections.forEach(section => {
+        const sectionScrollTop = section.offsetTop;
+        const sectionHeight = section.clientHeight; 
+        if(scrollY > (sectionScrollTop - sectionHeight /3 )){
+            nameSection = section.getAttribute("id");
+        }
+        
+    });
+    link.forEach(element => {
+        element.classList.remove("active");
+        if(element.classList.contains(nameSection)){
+            element.classList.add("active");
+        }
+    });
+
+}
+
+link.forEach(element =>{
+    element.addEventListener("click",function(e){
+        nav.classList.remove("nav-item-down");
+        bars.classList.remove("fa-times");
+        bars.classList.add("fa-bars");
+
+    })
+});
+
+
+//UP BUTTON//
+const buttonUp= document.getElementById("up");
+
+function clickUp(scroll){
+ buttonUp.addEventListener("click", function(){
+     if(scroll>200){
+         window.scrollTo(0,0);
+     }
+ })
+}
+ 
+function scrollUp(){
+    let scroll = window.scrollY;
+    
+    if(window.scrollY > 200){
+        buttonUp.classList.add("up-view");
+        clickUp(scroll);
+    }else {
+        buttonUp.classList.remove("up-view");
+    }
+ }
+
